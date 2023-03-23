2e18eb44e6264b8ca700ec18bba9dcaf336b7481
KAAN YARDIMCI <kaan.yardimci@yahoo.com.tr>
2023-03-23 17:46:13 +0300

    End of mobile design

--- JS/app.js
+++ JS/app.js
@@ -1,2 +1,12 @@
 "use strict";
-console.log("Hello World");
+
+const navBtn = document.querySelector("#navbar-btn");
+const closeBtn = document.querySelector("#close-btn");
+let navbar = document.querySelector(".nav-self");
+
+navBtn.addEventListener("click", () => {
+  navbar.classList.add("show-nav");
+});
+closeBtn.addEventListener("click", () => {
+  navbar.classList.remove("show-nav");
+});

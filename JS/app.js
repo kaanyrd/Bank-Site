651f0d01fcf231c7bba3e481eebc4e7d65050245
KAAN YARDIMCI <kaan.yardimci@yahoo.com.tr>
2023-03-27 10:50:42 +0300

    AUTO SCROLL

--- JS/app.js
+++ JS/app.js
@@ -7,6 +7,7 @@ const registerBtn = document.querySelectorAll(".register-btn");
 const signUpCloseBtn = document.getElementById("signUp-close-btn");
 const signUpSide = document.getElementById("signUp");
 const blur = document.querySelector("#signUp-blur");
+const moreBtn = document.querySelector(".learn-more-btn");
 
 navbarBtn.addEventListener("click", () => {
   asideBar.classList.add("show-aside");
@@ -36,4 +37,9 @@ signUpCloseBtn.addEventListener("click", (e) => {
   document.querySelector("body").style.overflow = "auto";
   signUpSide.className = "close";
   blur.className = "hidden";
+});
+//SCROLLING MAIN TO BOTTOM
+moreBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  document.getElementById("section-1").scrollIntoView({ behavior: "smooth" });
 });

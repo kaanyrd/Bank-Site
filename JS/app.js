0924254b2713c6207a7e72d0209b9515ff6df8ee
KAAN YARDIMCI <kaan.yardimci@yahoo.com.tr>
2023-03-27 09:59:12 +0300

    SIGNUP!

--- JS/app.js
+++ JS/app.js
@@ -3,6 +3,10 @@
 const navbarBtn = document.getElementById("navbar-btn");
 const closeBtn = document.getElementById("close-btn");
 const asideBar = document.getElementById("aside");
+const registerBtn = document.querySelectorAll(".register-btn");
+const signUpCloseBtn = document.getElementById("signUp-close-btn");
+const signUpSide = document.getElementById("signUp");
+const blur = document.querySelector("#signUp-blur");
 
 navbarBtn.addEventListener("click", () => {
   asideBar.classList.add("show-aside");
@@ -16,4 +20,20 @@ closeBtn.addEventListener("click", () => {
 var swiper = new Swiper(".mySwiper", {
   effect: "cards",
   grabCursor: true,
+});
+// SIGNUP OPEN
+registerBtn.forEach((index) => {
+  index.addEventListener("click", (e) => {
+    e.preventDefault();
+    signUpSide.className = "open";
+    document.querySelector("body").style.overflow = "hidden";
+    blur.className = "blur";
+  });
+});
+// SIGNUP CLOSE
+signUpCloseBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  document.querySelector("body").style.overflow = "auto";
+  signUpSide.className = "close";
+  blur.className = "hidden";
 });
